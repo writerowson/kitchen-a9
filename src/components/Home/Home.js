9db8414f1b0829bfa052b8e1e597927eac10593f
coderowson <runurowson@gmail.com>
2022-04-26 23:34:53 +0600

    ssd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,12 @@ import CustomLink from '../CustomLimk/CustomLink';
 const Home = () => {
     const [reviews, setReview] = UseReview([])
     return (
-        <div className='container-fluid'>
+        <div className='container'>
             <div className='row d-flex  mb-5'>
                 <div className='col-lg-5 col-12 mb-5'>
-                    <img src={img} width="400PX" alt="" />
+                    <img src={img} width="350PX" alt="" />
                 </div>
-                <div className='col-lg-7 col-12 text-start mt-1'>
+                <div className='col-lg-7 col-12 text-start mt-1 pe-3'>
                     <h1>Get Your Kitchen Appliances</h1>
                     <h1 >Set Your <span style={{ color: 'RGB(220,77,1)' }}>  Cooking Express</span></h1>
                     <p className='mt-3'> In this modern life  all busy in 24/7. For this busy life kitchen is the most untouchable part of every house. However, it's not any healthy solution. Modern kitchen accessories can be a healthy solution of time management and healthy food, That's why we are here </p>
@@ -32,6 +32,13 @@ const Home = () => {
                             >  </DisplayReview></div>)
 
                         }
+                        {/* {
+                            reviews?.slice(0, 3)?.map(review => <div className=''> <DisplayReview
+                                key={review._id}
+                                review={review}
+                            >  </DisplayReview></div>)
+
+                        } */}
                     </div>
                     <button style={{ backgroundColor: 'RGB(220,77,1)' }} type="button" className="btn  fw-bold mt-3"> <CustomLink className='text-light' to='/review' >See all reviews</CustomLink> </button>
                 </div>
